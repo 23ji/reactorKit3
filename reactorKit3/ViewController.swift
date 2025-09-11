5e968a34cdd7f653d89d53f53c9b1b2efe245947
23ji <isj4194@naver.com>
2025-09-11 11:02:57 +0900

    reactor 사용 설명

--- reactorKit3/ViewController.swift
+++ reactorKit3/ViewController.swift
@@ -21,10 +21,11 @@ final class ViewController: UIViewController, View {
   
   
   func bind(reactor: ViewReactor) {
+    // 액션 보내기 (화면 뜨면 initialize 실행)
     reactor.action.onNext(.initialize)
     print("isInitialized :", reactor.initialState.isInitialized)
 
-    
+    // state 구독
     reactor.state.map { $0.isInitialized }
       .subscribe(onNext: { value in
         print("🔥 isInitialized changed to:", value)
