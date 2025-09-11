e5cf4f79118005a2fe30926616a65f395ba06e6f
23ji <isj4194@naver.com>
2025-09-11 10:43:41 +0900

    ViewController에서 Reactor 사용

--- reactorKit3/ViewController.swift
+++ reactorKit3/ViewController.swift
@@ -5,12 +5,28 @@
 //  Created by 23ji on 9/9/25.
 //
 
+import ReactorKit
+import RxSwift
+
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController, View {
+  
+  var disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     self.view.backgroundColor = .red
+  }
+  
+  
+  func bind(reactor: ViewReactor) {
+    reactor.action.onNext(.initialize)
+    
+    reactor.state.map { $0.isInitialized }
+      .subscribe(onNext: { value in
+        print("isInitialized:", value)
+      })
+      .disposed(by: disposeBag)
   }
 }
